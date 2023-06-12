6304f8909960d7f12537d010c354243bc4fc01ea
Tanvir AR <tanvir.ar.47@gmail.com>
2023-06-12 21:44:39 +0600

    Update settings.py

--- musify_backend/settings.py
+++ musify_backend/settings.py
@@ -85,23 +85,23 @@ WSGI_APPLICATION = 'musify_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'verceldb',
-#         'USER': 'default',
-#         'PASSWORD': 'ps7MzuoHa0kG',
-#         'HOST': 'ep-shy-bonus-085947-pooler.ap-southeast-1.postgres.vercel-storage.com',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'verceldb',
+        'USER': 'default',
+        'PASSWORD': 'ps7MzuoHa0kG',
+        'HOST': 'ep-shy-bonus-085947-pooler.ap-southeast-1.postgres.vercel-storage.com',
+        'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
