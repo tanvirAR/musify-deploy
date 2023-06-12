9bc76d3b606bfb89bc72df1c7aa07d614f58a91c
Tanvir AR <tanvir.ar.47@gmail.com>
2023-06-12 22:31:47 +0600

    Update settings.py

--- musify_backend/settings.py
+++ musify_backend/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'musify_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'verceldb',
         'USER': 'default',
         'PASSWORD': 'ps7MzuoHa0kG',
