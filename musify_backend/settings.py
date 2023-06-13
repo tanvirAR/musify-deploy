926067dcb61f96ee461940bab1e472a0885564e2
tanvirAR <tanvir.ar.47@gmail.com>
2023-06-13 09:50:35 +0600

    deploy

--- musify_backend/settings.py
+++ musify_backend/settings.py
@@ -95,14 +95,17 @@ WSGI_APPLICATION = 'musify_backend.wsgi.application'
 #         'PORT': '5432',
 #     }
 # }
+import os
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        
     }
 }
 
+# 'NAME': BASE_DIR / 'db.sqlite3',
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
