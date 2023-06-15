a40a3ef7e605ef71a2e6964ab613b3d8f758fdf3
Tanvir AR <tanvir.ar.47@gmail.com>
2023-06-15 13:03:14 +0600

    Update settings.py

--- musify_backend/settings.py
+++ musify_backend/settings.py
@@ -159,7 +159,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
-    '1168-103-217-111-239.ngrok-free.app', 
+    '5ac9-103-217-111-239.ngrok-free.app', 
     'musify-deploy-production-2297.up.railway.app',
     
 ]
